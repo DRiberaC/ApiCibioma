c97b502d738d236c25b4bf936c181377f1856c3b
Diego Ribera <47877090+DRiberaC@users.noreply.github.com>
2021-09-10 22:03:25 -0400

    Revert "Update web.php"
    This reverts commit a45b53720f0f28fe895997e173871259c2c77e81.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth/login');
+    return view('welcome');
 });
 Route::group(['middleware' => ['auth']], function () {
     Route::get('home', [DatosController::class,"index"])->name('home.index');
